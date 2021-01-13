d2bb6e5d6d55c12ec92af147fbbcec925f1797a9
Reny10 <Reny10@users.noreply.github.com>
2021-01-13 19:21:16 +0000

    Updates

---
+++ precache-manifest.c4624a92f3bd8ff0627e5cae627cfc99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cecd8e0ad8a041a0ff103bc8db161f",
+    "url": "/widgets/index.html"
+  },
+  {
+    "revision": "11e16dafde9f2727ef6d",
+    "url": "/widgets/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "66256ae48ea4071fa009",
+    "url": "/widgets/static/js/2.114da5da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/widgets/static/js/2.114da5da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e16dafde9f2727ef6d",
+    "url": "/widgets/static/js/main.80da2d34.chunk.js"
+  },
+  {
+    "revision": "a3b7248df9ec66a8e8f5",
+    "url": "/widgets/static/js/runtime-main.4d12c3f6.js"
+  }
+]);
